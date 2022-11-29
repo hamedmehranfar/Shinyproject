d09df09f99008ae286a578a64e279dfa3dcf1873
Mehranfar  Hamed <hmehranfar@ethz.ch>
2022-11-29 14:52:05 +0100

    new_script_for_data_prepration

---
+++ data_prepration.R
@@ -0,0 +1,10 @@
+# This script is made to make the data ready to use in the report
+# This block is dedicated to processing the deterioration data
+#______________________________________________________________
+
+# Data_Input 
+
+library(readxl)
+bridge_data <- read_excel(paste(location, "Data", "ZolBgg-xmp.xlsx", sep = "/"), sheet = "CsEvo")
+
+# Update the header with the corrected year
