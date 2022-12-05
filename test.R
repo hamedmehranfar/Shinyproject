214eda6ed2ca95739c3a96d9b93e72c76def094c
Mehranfar  Hamed <hmehranfar@ethz.ch>
2022-12-05 16:01:36 +0100

    test

--- test.R
+++ test.R
@@ -8,7 +8,8 @@ bar_plot_1
 
 bar_data_2 <- bar_data %>% count(Type, CS) %>% group_by(Type) %>% mutate(prob = (n/sum(n)*100))
 bar_plot_2<- plot_ly(bar_data_2, x = ~Type, y = ~prob , color = ~CS ,type = "bar",
-                     text = ~prob %>% round() ) %>% layout(yaxis = list(ticksuffix = "%") )
+                     text = ~prob %>% round() ) %>% layout(yaxis = list(ticksuffix = "%") ) 
+
 bar_plot_2
 
 
