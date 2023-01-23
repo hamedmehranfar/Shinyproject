14ba63b67c0dbc16eb07c165cd1d4dfb19109dab
jonas121991 <78582857+jonas121991@users.noreply.github.com>
2023-01-23 15:08:44 +0100

    Final Update by Jonas

--- bridge_status_shiny.R
+++ bridge_status_shiny.R
@@ -3,12 +3,20 @@
 ### Calculation of working hours, concrete use and CO2 emissions ###
 ####################################################################
 
+# Second Step: Illustration of the annual input demand through gg-plot
+
+# Through our application, we offer a easy construction planning possibility to distribute the working efforts over an observation period.
+
 ### packages ###
 
 library(readxl)
 library(dplyr)
 library(data.table)
 library(ggplot2)
+library(tidyr)
+library(shiny)
+
+
 
 ### Data_Input ### 
 
@@ -49,15 +57,6 @@ concrete_use <- 5000  # tonnes
 CO2_emissions <- 2000 # tonnes
 
 
-# Each imaginary input has the following prices 
-
-working_hour_price <- 30   # CHF/hour
-
-concrete_price <- 20       # CHF/tonne
-
-CO2_emissions <- 50        # CHF/tonne
-
-
 
 ##################################
 ##################################
@@ -66,14 +65,14 @@ CO2_emissions <- 50        # CHF/tonne
 # Construction requirements and cots for each bridge 
 
 
-bridge_construction <- bridge_status[,c(1,18:31)]
+bridge_construction_hours <- bridge_status[,c(1,18:31)]
 
 
 # Calculation of input requirements of each bridge
 
 # plot 1: working hours
 
-bridge_construction_hours <- bridge_construction %>% 
+bridge_construction_hours <- bridge_construction_hours %>% 
   mutate_at(vars(constr_year1:constr_year14),
             .funs = funs(. * 1000))
 
@@ -82,39 +81,25 @@ bridge_construction_hours <- setnames(bridge_construction_hours,
          old = c('constr_year1','constr_year2','constr_year3','constr_year4','constr_year5','constr_year6',
                  'constr_year7','constr_year8','constr_year9','constr_year10','constr_year11','constr_year12',
                  'constr_year13','constr_year14'), 
-         new = c('hours_y1','hours_y2','hours_y3','hours_y4','hours_y5','hours_y6',
-                 'hours_y7','hours_y8','hours_y9','hours_y10','hours_y11','hours_y12',
-                 'hours_y13','hours_y14'))
+         new = c('y1','y2','y3','y4','y5','y6',
+                 'y7','y8','y9','y10','y11','y12',
+                 'y13','y14'))
 
 
-bridge_construction_hours1 <- melt(setDT(bridge_construction_hours), id.vars = c("MixName"))
-
- 
+hours_long <- gather(bridge_construction_hours, year, Working_Hours, y1:y14, factor_key=TRUE)
 
 
-# plot 2: costs for working hours
-
-bridge_construction_hours_costs <- bridge_construction_hours %>% 
-  mutate_at(vars(hours_y1:hours_y14),
-            .funs = funs(. * 30))
-
-
-setnames(bridge_construction_hours_costs, 
-         old = c('hours_y1','hours_y2','hours_y3','hours_y4','hours_y5','hours_y6',
-                 'hours_y7','hours_y8','hours_y9','hours_y10','hours_y11','hours_y12',
-                 'hours_y13','hours_y14'), 
-         new = c('costs_hours_y1','costs_hours_y2','costs_hours_y3','costs_hours_y4','costs_hours_y5','costs_hours_y6',
-                 'costs_hours_y7','costs_hours_y8','costs_hours_y9','costs_hours_y10','costs_hours_y11','costs_hours_y12',
-                 'costs_hours_y13','costs_hours_y14'))
+hours_plot <- ggplot(data=hours_long, aes(x=year, y=Working_Hours, group=MixName)) +
+  geom_line()
 
 
 
 
-# plot 3: concrete use 
+# plot 2: concrete use 
 
-bridge_construction <- bridge_status[,c(1,18:31)]
+bridge_construction_concrete <- bridge_status[,c(1,18:31)]
 
-bridge_construction_concrete <- bridge_construction %>% 
+bridge_construction_concrete <- bridge_construction_concrete %>% 
   mutate_at(vars(constr_year1:constr_year14),
             .funs = funs(. * 1000))
 
@@ -123,27 +108,42 @@ setnames(bridge_construction_concrete,
          old = c('constr_year1','constr_year2','constr_year3','constr_year4','constr_year5','constr_year6',
                  'constr_year7','constr_year8','constr_year9','constr_year10','constr_year11','constr_year12',
                  'constr_year13','constr_year14'), 
-         new = c('concrete_y1','concrete_y2','concrete_y3','concrete_y4','concrete_y5','concrete_y6',
-                 'concrete_y7','concrete_y8','concrete_y9','concrete_y10','concrete_y11','concrete_y12',
-                 'concrete_y13','concrete_y14'))
+         new = c('y1','y2','y3','y4','y5','y6',
+                 'y7','y8','y9','y10','y11','y12',
+                 'y13','y14'))
+
+concrete_long <- gather(bridge_construction_concrete, year, Concrete_Use, y1:y14, factor_key=TRUE)
+
+
+concrete_plot <- ggplot(data=concrete_long, aes(x=year, y=Concrete_Use, group=MixName)) +
+  geom_line()
 
 
 
-# plot 4: costs for concrete use
-
-bridge_construction_concrete_costs <- bridge_construction_concrete %>% 
-  mutate_at(vars(concrete_y1:concrete_y14),
-            .funs = funs(. * 20))
 
 
-setnames(bridge_construction_concrete_costs, 
-         old = c('concrete_y1','concrete_y2','concrete_y3','concrete_y4','concrete_y5','concrete_y6',
-                 'concrete_y7','concrete_y8','concrete_y9','concrete_y10','concrete_y11','concrete_y12',
-                 'concrete_y13','concrete_y14'), 
-         new = c('costs_concrete_y1','costs_concrete_y2','costs_concrete_y3','costs_concrete_y4','costs_concrete_y5','costs_concrete_y6',
-                 'costs_concrete_y7','costs_concrete_y8','costs_concrete_y9','costs_concrete_y10','costs_concrete_y11','costs_concrete_y12',
-                 'costs_concrete_y13','costs_concrete_y14'))
+# plot 3: CO2 
 
+bridge_construction_co2 <- bridge_status[,c(1,18:31)]
+
+bridge_construction_co2 <- bridge_construction_co2 %>% 
+  mutate_at(vars(constr_year1:constr_year14),
+            .funs = funs(. * 2000))
+
+
+setnames(bridge_construction_co2, 
+         old = c('constr_year1','constr_year2','constr_year3','constr_year4','constr_year5','constr_year6',
+                 'constr_year7','constr_year8','constr_year9','constr_year10','constr_year11','constr_year12',
+                 'constr_year13','constr_year14'), 
+         new = c('y1','y2','y3','y4','y5','y6',
+                 'y7','y8','y9','y10','y11','y12',
+                 'y13','y14'))
+
+co2_long <- gather(bridge_construction_co2, year, Concrete_Use, y1:y14, factor_key=TRUE)
+
+
+co2_plot <- ggplot(data=co2_long, aes(x=year, y=Concrete_Use, group=MixName)) +
+  geom_line()
 
 
 
