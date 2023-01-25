739605bc52b0eaeb26f94421d976e0bcf31203c8
hitesh-ethz <hiteshu.iitr@gmail.com>
2023-01-25 16:46:48 +0100

    updated app

--- apps/emission/app.R
+++ apps/emission/app.R
@@ -4,21 +4,21 @@ library(ggplot2)
 ui <- fluidPage(
   
   # Application title
-  titlePanel("CO2 Emissions Projection by 2050"),
+  titlePanel("CO2 Emissions Projection by 2050 and How each individual can contribute in achieving net-zero"),
   
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
       sliderInput("R",
-                  "No. of trees per month",
+                  "No. of trees planted per Year",
                   min = 0,
-                  max = 150,
-                  value = 1),
+                  max = 100,
+                  value = 10),
       sliderInput("f",
-                  "Amount of food waste avoided",
+                  "Amount of food waste avoided in a Year",
                   min = 0,
-                  max = 10,
-                  value = 1)
+                  max = 500,
+                  value = 25)
     ),
     
     # Show a plot of the generated distribution
@@ -40,13 +40,13 @@ server <- function(input, output) {
     s<- 1:28
     X<- 1:400
     population <- 0.007880000000
-    V = cdioxide/(population*input$R*25*12*s*input$f*2.5*12*s)
-    
+    V = cdioxide-(population*(input$R+(s-1)*input$R)*25*s+population*input$f*2.5*s)
+    P = population*input$R*25*s+population*input$f*2.5
     # draw the plot with the specified number of bins
-    plot(xValue,V, col = 'darkgray',
+    plot(xValue,cdioxide, col = 'darkgray',
          xlab = 'Year',
-         main = 'Plot to see the net-Zero', bty="n",type="l")
-    lines(xValue,cdioxide)
+         main = 'Plot to compare the combination of trees planted and food wastage avoided in order to achieve net zero', bty="n",type="l")
+    lines(xValue,P)
   })
 }
 
